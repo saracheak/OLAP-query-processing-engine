ca283475cba792264a0a35ed47581287be081c2e
Shasha Alvares <shashaalvares@mac.mynetworksettings.com>
2026-04-25 21:16:55 -0400

    got rid of connection logic in convert_to_mf_struct() bc it should be its own function

--- engine.py
+++ engine.py
@@ -157,35 +157,9 @@ class Generator:
         for f in phi_params["F"]:
             select_attributes += f"\t\tself.{f} = 0\n"
         
-        mf_struct = f"""import psycopg2
-        try:
-            #opens the database 
-            conn = psycopg2.connect(
-                dbname="sales",
-                user="postgres",
-                password="password", #you may need to change your postgres password using ALTER USER postgres WITH PASSWORD 'password';
-                host="localhost"
-            )
-
-            cur = conn.cursor() #prepares to run the queries
-                
-            cur.execute("SELECT version();")
-            db_version = cur.fetchone() #retrieves data row by row equivalent to: "for row in cur:"
-            print(f"Connected to: {{db_version}}")
-
-            cur.execute("SELECT * FROM sales;") #execute sends the SQL query to PostgreSQL, and the columns retrieved are stored in the cursor
-
-            #cur.close()
-            #conn.close()
-        except Exception:
-            print("Failed to connect to the database")
-            exit(1)
-                
-        class MFStruct:
-            def __init__(self):
-                {select_attributes}
-
-        """
+        mf_struct = f"""class MFStruct:
+    def __init__(self):
+{select_attributes}"""
         return mf_struct
 
 class HelperFunctions:
